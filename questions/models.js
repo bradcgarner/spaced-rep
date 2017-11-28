a36619c081b7109934ef402d9478aba7449d4d07
ericpcho <ericpcho@gmail.com>
2017-11-28 09:02:48 -0800

    added models for questions

--- questions/models.js
+++ questions/models.js
@@ -1,4 +1,15 @@
 'use strict';
 
 const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
 
+const QuestionSchema = mongoose.Schema({
+  brit: {type: String},
+  us: {type: String},
+  score: {type: Number},
+  nextIndex: {type: Number}
+});
+
+const Questions = mongoose.models.Questions || mongoose.model('Questions', QuestionSchema);
+
+module.exports = { Questions };
